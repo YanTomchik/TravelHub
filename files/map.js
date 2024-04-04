ee17dbe973332b41a6f4155381c236369cf7bb30
yantomchik <yan.tomchik@mail.ru>
2024-04-04 10:44:33 +0300

    init 040424

--- files/map.js
+++ files/map.js
@@ -1,33 +1,122 @@
+const formData = new FormData();
+formData.append("PropertySearchForm[location]", "2621");
+formData.append("PropertySearchForm[checkinDate]", "10.04.2024");
+formData.append("PropertySearchForm[checkoutDate]", "12.04.2024");
+formData.append("PropertySearchForm[guests]", JSON.stringify([{ "adults": 2 }]));
+formData.append("PropertySearchForm[partner]", "11090");
+formData.append("PropertySearchForm[map]", "true");
+
+// const formData = new FormData();
+// formData.append("PropertySearchForm[location]", "6");
+// formData.append("PropertySearchForm[checkinDate]", "10.07.2024");
+// formData.append("PropertySearchForm[checkoutDate]", "20.07.2024");
+// formData.append("PropertySearchForm[guests]", JSON.stringify([{ "adults": 2 }]));
+// formData.append("PropertySearchForm[partner]", "11115");
+// formData.append("PropertySearchForm[map]", "true");
+
+
+// const getHotelsInfo = async () => {
+//   try {
+//     const hotelsInfo = await fetch('https://travelhub.by/hotels/search-map', {
+//           method: 'POST',
+//           body: formData
+//         })
+//         .then(response => {
+//           if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//           }
+//           return response.json();
+//         })
+//         .then(result => {
+//           console.log(result)
+//           return result;
+//         })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
 let currentOpenMarker = null;
 
-function initMap() {
-  const center = {
-    lat: 37.43238031167444,
-    lng: -122.16795397128632,
-  };
-  const map = new google.maps.Map(document.getElementById("map-dashoard-wrapper"), {
-    zoom: 11,
-    center,
-    mapId: "4504f8b37365c3d0",
+const loaderDiv = document.getElementById('loader-map');
+
+function initMapHotels() {
+
+  fetch('https://travelhub.by/hotels/search-map', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(result => {
+
+    
+    let advancedMarkerViews = []
+    const center = {
+      lat: 40.84,
+      lng: -73.97,
+    };
+
+    
+
+    const map = new google.maps.Map(document.getElementById("map-dashoard-wrapper"), {
+      zoom: 13,
+      center,
+      mapId: "4504f8b37365c3d0",
+      // styles: customMapStyles
+    });
+
+    const dataHotelsObj = result.data;
+    const currencyName = result.currency;
+    const countHotels = result.count;
+    
+    for (const property of dataHotelsObj){
+
+      const nameHotel = property.name;
+      
+      const position = {
+        lat: parseFloat(property.latitude),
+        lng: parseFloat(property.longitude)
+      }
+
+      
+
+      const advancedMarkerView = new google.maps.marker.AdvancedMarkerView({
+        map,
+        content: buildContent(property, currencyName, countHotels),
+        position: position,
+        title: nameHotel,
+        // optimized: true,
+      });
+
+  
+      const element = advancedMarkerView.element;
+      
+      element.style.zIndex = '-1'
+      element.addEventListener("click", () => {
+        toggleContentVisibility(advancedMarkerView);
+        
+      });
+      advancedMarkerViews.push(advancedMarkerView);
+
+      buildLeftContent(property,currencyName, countHotels);
+    }
+
+    google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
+      loaderDiv.style.display = 'none';
+    });
+  })
+  .catch(error => {
+    
+    console.error('There has been a problem with your fetch operation:', error);
   });
 
-  for (const property of properties) {
-    const advancedMarkerView = new google.maps.marker.AdvancedMarkerView({
-      map,
-      content: buildContent(property),
-      position: property.position,
-      title: property.description,
-    });
-
-    const element = advancedMarkerView.element;
-    
-    element.style.zIndex = '-1'
-    element.addEventListener("click", () => {
-      toggleContentVisibility(advancedMarkerView);
-      
-    });
-  }
 }
+
 
 function toggleContentVisibility(markerView) {
     
@@ -43,36 +132,35 @@ function toggleContentVisibility(markerView) {
   
 }
 
-
-function buildContent(property) {
+function buildContent(property, currencyName, countHotels) {
   const content = document.createElement("div");
 
   content.classList.add("property");
   content.innerHTML = `
     <div class="icon">
       <div class="map-marker-description">
-        ${property.price}
+        ${property.priceTotal}${currencyName}
       </div>
     </div>
 
     <div class="details">
       <div class="marker-popup-wrapper">
         <div class="marker-popup-header-wrapper">
-          <div class="market-popup-header-img-wrapper">
+          <div class="market-popup-header-img-wrapper" style="background-image:url(${property.image});">
           </div>
           <div class="marker-popup-header-info">
             <div class="marker-popup-header-title-wrapper">
               <div class="marker-popup-header-title">
                 ${property.name}
               </div>
+              
               <div class="marker-popup-header-title-stars-list">
-                <img src="./images/star-marker.svg" alt="star" class="marker-popup-header-title-star-item">
-                <img src="./images/star-marker.svg" alt="star" class="marker-popup-header-title-star-item">
+              ${buildRatingBlock(property.rating)}
               </div>
             </div>
             <div class="marker-popup-header-description-wrapper">
               <div class="marker-popup-header-description-rate">
-                ${property.bed}
+                ${property.rating}
               </div>
               <div class="marker-popup-header-description">
                 Рейтинг гостей
@@ -84,199 +172,155 @@ function buildContent(property) {
           </a>
         </div>
         <div class="marker-popup-footer-info">
-          <div class="marker-popup-footer-description-wrapper">
-            <div class="marker-popup-footer-description">
-              <div class="marker-popup-footer-description-main">
-                Всего (нетто цена):
-              </div>
-              <div class="marker-popup-footer-description-price">
-                ${property.bed}$
-              </div>
-            </div>
-            <div class="marker-popup-footer-description">
-              <div class="marker-popup-footer-description-main">
-                Всего (включая налоги и сборы):
-              </div>
-              <div class="marker-popup-footer-price-wrapper">
+                  <div class="marker-popup-footer-description-wrapper">
+                    <div class="marker-popup-footer-description">
+                      <div class="marker-popup-footer-description-main">
+                        Всего (нетто цена):
+                      </div>
+                      <div class="marker-popup-footer-description-price">
+                        ${property.priceNightly} ${currencyName}
+                      </div>
+                    </div>
+                    <div class="marker-popup-footer-description">
+                      <div class="marker-popup-footer-description-main">
+                        Всего (включая налоги и сборы):
+                      </div>
+                      <div class="marker-popup-footer-description-price">
+                      ${property.priceStrike} ${currencyName}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="marker-popup-footer-price-wrapper">
                     <div class="marker-popup-footer-price-alert">
                       
                     </div>
                     <div class="marker-popup-footer-price">
-                    ${property.bed}$
+                      за ночь ${property.priceTotal} ${currencyName}
                     </div>
                     
                   </div>
-            </div>
-          </div>
-          <div class="marker-popup-footer-price">
-            за ночь ${property.bed}$
-          </div>
-        </div>
+                </div>
       </div>
     </div>
   `;
   return content;
 }
-  
-  const properties = [
-    {
-      address: "215 Emily St, MountainView, CA",
-      name:"Mamma Mia",
-      description: "Single family house with modern design",
-      price: "$ 3,889,000",
-      type: "home",
-      bed: 5,
-      bath: 4.5,
-      size: 300,
-      position: {
-        lat: 37.50024109655184,
-        lng: -122.28528451834352,
-      },
-    },
-    {
-      address: "108 Squirrel Ln &#128063;, Menlo Park, CA",
-      name:"Mamma Mia",
-      description: "Townhouse with friendly neighbors",
-      price: "$ 3,050,000",
-      type: "building",
-      bed: 4,
-      bath: 3,
-      size: 200,
-      position: {
-        lat: 37.44440882321596,
-        lng: -122.2160620727,
-      },
-    },
-    {
-      address: "100 Chris St, Portola Valley, CA",
-      name:"Mamma Mia",
-      description: "Spacious warehouse great for small business",
-      price: "$ 3,125,000",
-      type: "warehouse",
-      bed: 4,
-      bath: 4,
-      size: 800,
-      position: {
-        lat: 37.39561833718522,
-        lng: -122.21855116258479,
-      },
-    },
-    {
-      address: "98 Aleh Ave, Palo Alto, CA",
-      name:"Mamma Mia",
-      description: "A lovely store on busy road",
-      price: "$ 4,225,000",
-      type: "store-alt",
-      bed: 2,
-      bath: 1,
-      size: 210,
-      position: {
-        lat: 37.423928529779644,
-        lng: -122.1087629822001,
-      },
-    },
-    {
-      address: "2117 Su St, MountainView, CA",
-      name:"Mamma Mia",
-      description: "Single family house near golf club",
-      price: "$ 1,700,000",
-      type: "home",
-      bed: 4,
-      bath: 3,
-      size: 200,
-      position: {
-        lat: 37.40578635332598,
-        lng: -122.15043378466069,
-      },
-    },
-    {
-      address: "197 Alicia Dr, Santa Clara, CA",
-      name:"Mamma Mia",
-      description: "Multifloor large warehouse",
-      price: "$ 5,000,000",
-      type: "warehouse",
-      bed: 5,
-      bath: 4,
-      size: 700,
-      position: {
-        lat: 37.36399747905774,
-        lng: -122.10465384268522,
-      },
-    },
-    {
-      address: "700 Jose Ave, Sunnyvale, CA",
-      name:"Mamma Mia",
-      description: "3 storey townhouse with 2 car garage",
-      price: "$ 3,850,000",
-      type: "building",
-      bed: 4,
-      bath: 4,
-      size: 600,
-      position: {
-        lat: 37.38343706184458,
-        lng: -122.02340436985183,
-      },
-    },
-    {
-      address: "868 Will Ct, Cupertino, CA",
-      name:"Mamma Mia",
-      description: "Single family house in great school zone",
-      price: "$ 2,500,000",
-      type: "home",
-      bed: 3,
-      bath: 2,
-      size: 100,
-      position: {
-        lat: 37.34576403052,
-        lng: -122.04455090047453,
-      },
-    },
-    {
-      address: "655 Haylee St, Santa Clara, CA",
-      name:"Mamma Mia",
-      description: "2 storey store with large storage room",
-      price: "$ 2,500,000",
-      type: "store-alt",
-      bed: 3,
-      bath: 2,
-      size: 450,
-      position: {
-        lat: 37.362863347890716,
-        lng: -121.97802139023555,
-      },
-    },
-    {
-      address: "2019 Natasha Dr, San Jose, CA",
-      name:"Mamma Mia",
-      description: "Single family house",
-      price: "$ 2,325,000",
-      type: "home",
-      bed: 4,
-      bath: 3.5,
-      size: 500,
-      position: {
-        lat: 37.41391636421949,
-        lng: -121.94592071575907,
-      },
-    },
-  ];
-  
-  window.initMap = initMap;
+ 
+function buildLeftContent(property, currencyName, countHotels){
+  const mapDashboardCardsListWrapper = document.getElementById('map-dashboard-cards-list');
+  const headerMapCountElement = document.getElementById('map-dashboard-filter-header-description');
 
+  headerMapCountElement.innerHTML = `${countHotels} отеля в этой области`
+  
+  let flagrRefundable = property.refundable;
+  if(flagrRefundable == true){
+    flagrRefundable = 'Полный возврат'
+  }else{
+    flagrRefundable = '';
+  }
+  
+  const content = document.createElement("div");
+
+  content.classList.add("map-dashboard-card-item");
+  content.innerHTML = `
+  <div class="marker-popup-header-wrapper">
+                  <div class="market-popup-header-img-wrapper" style="background-image:url(${property.image});">
+                  </div>
+                  <div class="marker-popup-header-info">
+                    <div class="marker-popup-header-title-wrapper">
+                      <div class="marker-popup-header-title">
+                        ${property.name}
+                      </div>
+                      <div class="marker-popup-header-title-stars-list" id="marker-popup-header-title-stars-list">
+                        ${buildRatingBlock(property.rating)}
+                      </div>
+                    </div>
+                    <div class="marker-popup-header-description-wrapper">
+                      <div class="marker-popup-header-description-rate">
+                        ${property.rating}
+                      </div>
+                      <div class="marker-popup-header-description">
+                        Рейтинг гостей
+                      </div>
+                    </div>
+                    <div class="marker-popup-refundable-description">
+                        ${flagrRefundable}
+                      </div>
+                  </div>
+                  <a href="#" class="marker-popup-header-btn">
+                    <img src="./images/arrow-right-btn.svg" alt="">
+                  </a>
+                </div>
+                <div class="marker-popup-footer-info">
+                  <div class="marker-popup-footer-description-wrapper">
+                    <div class="marker-popup-footer-description">
+                      <div class="marker-popup-footer-description-main">
+                        Всего (нетто цена):
+                      </div>
+                      <div class="marker-popup-footer-description-price">
+                      ${property.priceNightly} ${currencyName}
+                      </div>
+                    </div>
+                    <div class="marker-popup-footer-description">
+                      <div class="marker-popup-footer-description-main">
+                        Всего (включая налоги и сборы):
+                      </div>
+                      <div class="marker-popup-footer-description-price">
+                      ${property.priceStrike} ${currencyName}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="marker-popup-footer-price-wrapper">
+                    <div class="marker-popup-footer-price-alert">
+                      
+                    </div>
+                    <div class="marker-popup-footer-price">
+                      за ночь ${property.priceTotal} ${currencyName}
+                    </div>
+                    
+                  </div>
+                </div>
+  `;
+  mapDashboardCardsListWrapper.appendChild(content)
+}
+
+function buildRatingBlock (rating){
+
+  let stars = ''
+  let ratingToItterate = 0;
+
+  if (rating - Math.floor(rating) === 0.5) {
+    ratingToItterate = Math.floor(rating);
+  } else {
+    ratingToItterate =  Math.round(rating);
+  }
+  
+  for(let i=0; i < ratingToItterate; i++){
+    stars += '<img src="./images/star-marker.svg" alt="star" class="marker-popup-header-title-star-item">';
+  }
+
+  
+  return stars;
+
+}
 
 const mapShowSearch = document.getElementById('result-amount-map-search-wrapper')
 const mapDashboardWrapper = document.getElementById('map-dashboard-main-wrapper-fade')
 mapShowSearch.addEventListener('click', function(){
-  mapDashboardWrapper.classList.toggle('active')
+  mapDashboardWrapper.classList.toggle('active');
+  initMapHotels()
+  
+  
+  loaderDiv.style.display = 'block';
 })
 
 const mapDashboardFilterWrapper = document.getElementById('map-dashboard-filter-wrapper');
 const mapDashboardFilterBtn = document.getElementById('map-dashboard-filter-header-btn');
 const mapDashboardCardsListWrapper = document.getElementById('map-dashboard-cards-list');
-mapDashboardFilterBtn.addEventListener('click', function(){
-  mapDashboardFilterWrapper.classList.toggle('active')
-  mapDashboardCardsListWrapper.classList.toggle('hide')
-})
+// mapDashboardFilterBtn.addEventListener('click', function(){
+//   mapDashboardFilterWrapper.classList.toggle('active')
+//   mapDashboardCardsListWrapper.classList.toggle('hide')
+// })
 
 
 const closeFilterBlockbtn = document.getElementById('filters-block-submenus-header-btn');
@@ -290,3 +334,8 @@ const closeMapDashboardMainWrapperBtn = document.getElementById('map-dashboard-c
 closeMapDashboardMainWrapperBtn.addEventListener('click', function(){
   mapDashboardWrapper.classList.toggle('active')
 })
+
+
+
+
+
