3c945af18f85e371255c9a302d4bef1a7bfa235d
yantomchik <yan.tomchik@mail.ru>
2024-06-17 22:33:15 +0300

    init add fetch function reward

--- files/rewards-user.js
+++ files/rewards-user.js
@@ -1,121 +1,86 @@
-// alert()
-
-// Пример JSON данных
-const dataX = {
-    "payout": 1000,
-    "current": {
-        "id": 5,
-        "type": 0,
-        "title": "Серебряный",
-        "tier": "2",
-        "rewardType": "discount",
-        "rewardValue": "5.00",
-        "purchaseThreshold": "5000.00"
-    },
-    "next": {
-        "id": 6,
-        "type": 0,
-        "title": "Золотой",
-        "tier": "3",
-        "rewardType": "discount",
-        "rewardValue": "7.00",
-        "purchaseThreshold": "10000.00"
-    },
-    "all": [
-        {
-            "id": 1,
-            "type": 1,
-            "title": "Бронзовый",
-            "tier": "1",
-            "rewardType": "commission",
-            "rewardValue": "0.50",
-            "purchaseThreshold": "20000.00"
-        },
-        {
-            "id": 2,
-            "type": 1,
-            "title": "Серебряный",
-            "tier": "2",
-            "rewardType": "commission",
-            "rewardValue": "0.75",
-            "purchaseThreshold": "50000.00"
-        },
-        {
-            "id": 3,
-            "type": 1,
-            "title": "Золотой",
-            "tier": "3",
-            "rewardType": "commission",
-            "rewardValue": "1.00",
-            "purchaseThreshold": "100000.00"
-        }
-    ]
-};
-
 function formatNumber(value) {
-    return parseFloat(value).toString();
+    return parseFloat(value).toFixed(2);
 }
 
-// Функция для обновления блока
 function updateRewardsBlock(data) {
+    console.log(data)
     const currentStatus = data.current;
     const nextStatus = data.next;
-    let currentStatusToDraw = null;
-    let nextStatusToDraw = null;
 
-    if (currentStatus.title == "Бронзовый") {
-        currentStatusToDraw = 'bronze';
-    } else if (currentStatus.title == "Серебряный") {
-        currentStatusToDraw = 'silver';
-    } else if (currentStatus.title == "Золотой") {
-        currentStatusToDraw = 'gold';
-    } else {
-        currentStatusToDraw = 'default-style';
-    }
+    const tierStyles = {
+        "1": "bronze",
+        "2": "silver",
+        "3": "gold"
+    };
 
-    if (nextStatus.title == "Бронзовый") {
-        nextStatusToDraw = 'bronze';
-    } else if (nextStatus.title == "Серебряный") {
-        nextStatusToDraw = 'silver';
-    } else if (nextStatus.title == "Золотой") {
-        nextStatusToDraw = 'gold';
-    } else {
-        nextStatusToDraw = 'default';
-    }
+    const currentStatusToDraw = tierStyles[currentStatus.tier] || 'default-style';
+    const nextStatusToDraw = tierStyles[nextStatus.tier] || 'default';
+    
 
     if (currentStatusToDraw != 'default-style') {
-        // Обновление заголовка статуса
+
         document.getElementById('user-status-title').textContent = currentStatus.title.toUpperCase();
+        document.getElementById('reward-description').innerHTML = `+<b>${formatNumber(nextStatus.purchaseThreshold)}</b> USD до`;
 
-        // Обновление описания награды
-        document.getElementById('reward-description').innerHTML = `+<b>${formatNumber(nextStatus.purchaseThreshold)}</b> usd до`;
-
-        // Обновление статуса
-        let statusWrapper = document.getElementById('rewards-status-wrapper');
-        statusWrapper.innerHTML = `${nextStatus.title.toUpperCase()}`;
+        const statusWrapper = document.getElementById('rewards-status-wrapper');
+        statusWrapper.textContent = nextStatus.title.toUpperCase();
         statusWrapper.classList.add(nextStatusToDraw);
 
-        // Обновление прогресс-бара
-        let progressBar = document.getElementById('progress-bar-main');
-        // progressBar.className = `progress-bar-main ${statusToDraw}`;
-        progressBar.style.width = `${Math.min(100, (data.payout / nextStatus.purchaseThreshold) * 100)}%`;
-        progressBar.classList.add(currentStatusToDraw)
+        const progressBar = document.getElementById('progress-bar-main');
+        progressBar.style.width = `${Math.min(100, (data.payout / parseFloat(nextStatus.purchaseThreshold)) * 100)}%`;
+        progressBar.classList.add(currentStatusToDraw);
 
-
-        // Обновление изображения
-        let rewardIcon = document.getElementById('reward-icon');
-
+        const rewardIcon = document.getElementById('reward-icon');
         rewardIcon.src = `./images/${currentStatusToDraw}-plane-reward.svg`;
 
-        let rewardsUserMiddle = document.getElementById('rewards-user-middle');
-        rewardsUserMiddle.classList.add(currentStatusToDraw)
-    }else{
-        document.getElementById('rewards-user-container').classList.add('default-style')
+        const rewardsUserMiddle = document.getElementById('rewards-user-middle');
+        rewardsUserMiddle.classList.add(currentStatusToDraw);
+
+        //header info
+        const descriptionInfoDesktopAccountWrapper = document.querySelector('.description-info-desktop-account-wrapper');
+        descriptionInfoDesktopAccountWrapper.classList.add(currentStatusToDraw)
+
+        const descriptionInfoDesktopAccount = document.querySelector('.description-info-desktop-account');
+        descriptionInfoDesktopAccount.innerHTML = `${currentStatus.title} статус`;
+
+
+        const descriptionAccountImg = document.querySelector('.description-info-desktop-account-img');
+        descriptionAccountImg.src = `./images/${currentStatusToDraw}-plane-reward.svg`;
+
+    } else {
+        document.getElementById('rewards-user-container').classList.add('default-style');
     }
-
-
 }
 
-// Вызов функции для обновления блока
-updateRewardsBlock(dataX);
+async function fetchDataAndUpdate() {
+    try {
+        const response = await fetch('https://travelhub.by/user/loyalty');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        localStorage.setItem('loyaltyData', JSON.stringify(data));
+        localStorage.setItem('lastFetchTime', Date.now());
+        
+        updateRewardsBlock(data);
+    } catch (error) {
+        document.getElementById('rewards-user-container').classList.add('default-style');
+        console.error('Error fetching data:', error);
+        
+    }
+}
 
+function init() {
+    const storedData = localStorage.getItem('loyaltyData');
+    const lastFetchTime = localStorage.getItem('lastFetchTime');
+
+    if (storedData && lastFetchTime && Date.now() - lastFetchTime < 3600000) { // 1 hour in milliseconds
+        updateRewardsBlock(JSON.parse(storedData));
+    } else {
+        fetchDataAndUpdate();
+    }
+
+    fetchDataAndUpdate();
+}
+
+init();
