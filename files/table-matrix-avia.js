91c04545017febf711986c64233d3f8b8ffdd62f
yantomchik <yan.tomchik@mail.ru>
2024-07-30 10:41:40 +0300

    init add click on cell matrix and fix bugs

--- files/table-matrix-avia.js
+++ files/table-matrix-avia.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    let currentStartDate = new Date('2024-08-01');  // Example start date
-    let currentEndDate = new Date('2024-08-08');    // Example end date
-    const today = new Date(); // Current date
+    
+
+    const todayDate = new Date(); // Current date
+    const extratextSearchMatrix = document.getElementById('extratextSearchMatrix')
 
     let tableData, departureDates, returnDates;
     const maxColumns = 7; // Number of columns to display at once
@@ -9,17 +10,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     const theadRow = document.querySelector('thead tr');
     const tbody = document.querySelector('tbody');
 
-    await fetchData();
+    let selectedDepartureDate = null;
+    let selectedReturnDate = null;
 
-    async function fetchData() {
+    await fetchDataMatrix();
+
+    async function fetchDataMatrix() {
+
         document.getElementById('loader-compare-table').style.display = 'block';
+
+        let currentStartDate = new Date(parseDate(datepickerInputFrom.value));  // Example start date
+        let currentEndDate = new Date(parseDate(datepickerInputTo.value));    // Example end date
+
+        let locationFrom = $('#flightsearchform-locationfrom').val();
+        let locationTo = $('#flightsearchform-locationto').val()
         const dateFromFetch = formatDate(currentStartDate)
         const dateToFetch = formatDate(currentEndDate)
 
-        console.log(dateFromFetch)
-        console.log(dateToFetch)
-        const apiUrl = `https://api.travelhub.by/flight/comparison-table?route=trip&locationFrom=MSQ&locationTo=MOW&adults=1&period=${dateFromFetch};${dateToFetch}&currency=${USER_CURRENCY}`;
-        console.log('Fetching data from:', apiUrl); // Debug log
+        // console.log(dateFromFetch)
+        // console.log(dateToFetch)
+        const apiUrl = `https://api.travelhub.by/flight/comparison-table?route=trip&locationFrom=${locationFrom}&locationTo=${locationTo}&adults=1&period=${dateFromFetch};${dateToFetch}&currency=${USER_CURRENCY}`;
+        // console.log('Fetching data from:', apiUrl); // Debug log
 
         const response = await fetch(apiUrl, {
             headers: {
@@ -29,7 +40,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         const data = await response.json();
         tableData = data.result;
-        console.log(tableData);
+        // console.log(tableData);
 
         departureDates = [...new Set(tableData.map(item => item.from))].sort((a, b) => new Date(a.split('.').reverse().join('-')) - new Date(b.split('.').reverse().join('-')));
         returnDates = [...new Set(tableData.map(item => item.to))].sort((a, b) => new Date(a.split('.').reverse().join('-')) - new Date(b.split('.').reverse().join('-')));
@@ -92,6 +103,19 @@ document.addEventListener('DOMContentLoaded', async () => {
                 } else {
                     td.textContent = '-';
                 }
+
+                // Add click event listener to each cell
+                td.addEventListener('click', () => {
+                    selectedDepartureDate = depDate;
+                    selectedReturnDate = retDate;
+
+                    datepicker.selectDate([parseDate(selectedDepartureDate), parseDate(selectedReturnDate)]);
+                    extratextSearchMatrix.classList.add('active')
+
+                    // console.log(`Selected Departure Date: ${parseDate(selectedDepartureDate)}`);
+                    // console.log(`Selected Return Date: ${parseDate(selectedReturnDate)}`);
+                });
+
                 tr.appendChild(td);
             });
 
@@ -174,30 +198,30 @@ document.addEventListener('DOMContentLoaded', async () => {
         const newStartDate = new Date(currentStartDate);
         newStartDate.setDate(newStartDate.getDate() - 1);
 
-        if (newStartDate > today) {
+        if (newStartDate > todayDate) {
             currentStartDate = newStartDate;
-            await fetchData();
+            await fetchDataMatrix();
         }
     });
 
     document.getElementById('next-departure').addEventListener('click', async () => {
         currentStartDate.setDate(currentStartDate.getDate() + 1);
-        await fetchData();
+        await fetchDataMatrix();
     });
 
     document.getElementById('prev-return').addEventListener('click', async () => {
         const newEndDate = new Date(currentEndDate);
         newEndDate.setDate(newEndDate.getDate() - 1);
 
-        if (newEndDate > today) {
+        if (newEndDate > todayDate) {
             currentEndDate = newEndDate;
-            await fetchData();
+            await fetchDataMatrix();
         }
     });
 
     document.getElementById('next-return').addEventListener('click', async () => {
         currentEndDate.setDate(currentEndDate.getDate() + 1);
-        await fetchData();
+        await fetchDataMatrix();
     });
 
     function formatDate(date) {
@@ -237,5 +261,19 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.querySelector('.compare-table-avia-header-wrapper .close-btn').addEventListener('click', () => {
         document.getElementById('compare-table-avia-container').classList.toggle('active');
     });
+
+    function parseDate(dateString) {
+        const [day, month, year] = dateString.split('.').map(Number);
+        return new Date(year, month - 1, day);
+    }
+
+    $('#flightsearchform-locationfrom').on('change', function () {
+        fetchDataMatrix();
+    
+    })
+    
+    $('#flightsearchform-locationto').on('change', function () {
+        fetchDataMatrix();
+    })
 
 });
