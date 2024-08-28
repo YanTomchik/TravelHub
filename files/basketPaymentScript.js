c5d427478b423d2ae8f752d32f26c3d4b3d4959f
yantomchik <yan.tomchik@mail.ru>
2024-08-28 23:23:59 +0300

    init add styles for politics

--- files/basketPaymentScript.js
+++ files/basketPaymentScript.js
@@ -3,19 +3,15 @@ const tabs = document.querySelectorAll('.payment-header-btn');
 
     tabs.forEach(tab => {
         tab.addEventListener('click', function () {
-            // Удаляем класс active у всех табов
             tabs.forEach(t => t.classList.remove('active'));
-            // Добавляем класс active к текущему табу
+
             this.classList.add('active');
 
-            // Скрываем все контенты
             contents.forEach(content => content.classList.remove('active'));
 
-            // Находим и показываем соответствующий контент по data-target атрибуту
             const targetId = this.getAttribute('data-target');
             const targetContent = document.getElementById(targetId);
 
-            // Добавляем класс active только нужному контенту
             if (targetContent) {
                 targetContent.classList.add('active');
             }
---
+++ files/travelPolitics.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelectorAll('.custom-select-politics').forEach(select => {
+        
+        const selectBox = select.querySelector('.select-box-politics');
+        const options = select.querySelector('.select-options-politics');
+        const label = select.querySelector('.select-label-politics');
+        
+        selectBox.addEventListener('click', function (event) {
+            event.stopPropagation(); 
+            
+            const isOptionsVisible = options.style.display === 'block';
+                        
+            document.querySelectorAll('.select-options-politics').forEach(opt => {
+                opt.style.display = 'none';
+            });
+            
+            if (!isOptionsVisible) {
+                options.style.display = 'block';
+            }
+        });
+
+        options.querySelectorAll('.option-politics').forEach(option => {
+            option.addEventListener('click', function () {
+                label.textContent = this.textContent; 
+                options.style.display = 'none'; 
+            });
+        });
+    });
+
+
+    // Находим все элементы с классом .info-politics-item
+    document.querySelectorAll('.info-politics-item').forEach(infoItem => {
+        // Находим заголовок и иконку внутри каждого элемента
+        const header = infoItem.querySelector('.header-info-politics-item');
+        const chevronContainer = header.querySelector('.header-title-chevron'); // Найти контейнер иконки
+        const chevron = chevronContainer.querySelector('img'); // Найти иконку внутри контейнера
+
+        header.addEventListener('click', function () {
+            // Проверяем текущее состояние видимости блоков
+            const isAnyGroupVisible = Array.from(infoItem.querySelectorAll('.group-info-politics-item'))
+                .some(group => group.style.display !== 'none');
+
+            // Переключаем видимость всех блоков .group-info-politics-item внутри текущего info-politics-item
+            infoItem.querySelectorAll('.group-info-politics-item').forEach(group => {
+                // Если хотя бы один блок видим, скрываем все, иначе показываем все
+                group.style.display = isAnyGroupVisible ? 'none' : 'block';
+            });
+
+            // Переключение класса для вращения иконки
+            chevron.classList.toggle('rotate-180', !isAnyGroupVisible);
+
+            // Скрываем блоки и сбрасываем вращение в других .info-politics-item
+            document.querySelectorAll('.info-politics-item').forEach(otherItem => {
+                if (otherItem !== infoItem) {
+                    // Скрыть все группы в других блоках
+                    otherItem.querySelectorAll('.group-info-politics-item').forEach(group => {
+                        // group.style.display = 'none';
+                    });
+
+                    // Удалить класс вращения с иконки в других заголовках
+                    const otherChevron = otherItem.querySelector('.header-title-chevron img');
+                    if (otherChevron) {
+                        otherChevron.classList.remove('rotate-180');
+                    }
+                }
+            });
+        });
+    });
+
+});
