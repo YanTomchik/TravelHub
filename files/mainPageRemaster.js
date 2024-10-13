bb910d5400c9452f2aef91a72ec7f8d40cd75a7a
yantomchik <yan.tomchik@mail.ru>
2024-10-14 02:22:05 +0300

    init add slider for logos

--- files/mainPageRemaster.js
+++ files/mainPageRemaster.js
@@ -70,10 +70,26 @@ window.addEventListener('scroll', function() {
         slidesToShow: 8,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2000,
         arrows: false,
         infinite: true,
-        rtl: false // Обычная прокрутка
+        rtl: false, // Обычная прокрутка
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            }
+          }
+        ] 
     });
 
     // Нижний слайдер - справа налево
@@ -81,10 +97,26 @@ window.addEventListener('scroll', function() {
         slidesToShow: 8,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2000,
         arrows: false,
         infinite: true,
-        rtl: true // Прокрутка справа налево
+        rtl: true, // Прокрутка справа налево
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            }
+          }
+        ] 
     });
 });
 
